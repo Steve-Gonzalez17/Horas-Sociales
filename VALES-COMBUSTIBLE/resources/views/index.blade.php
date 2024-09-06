a27d192173eec8c99b9c7e3c864878a842571fa9
Samuel Mejía <79532040+Samuel-Mejia@users.noreply.github.com>
2024-09-06 16:46:35 -0600

    123Probando

---
+++ VALES-COMBUSTIBLE/resources/views/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<P>Iva = %13</P>
+    
+</body>
+</html>
