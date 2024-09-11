79773df6b21b6c529b241b52ac56c699edef0033
Samuel Mejía <79532040+Samuel-Mejia@users.noreply.github.com>
2024-09-10 20:07:31 -0600

    Correcccion

--- VALES-COMBUSTIBLE/resources/views/index.blade.php
+++ VALES-COMBUSTIBLE/resources/views/index.blade.php
@@ -8,16 +8,13 @@
     <title>Document</title>
 </head>
 <body>
-
-
 <!-- Main modal -->
 <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 
 <div class="relative p-4 w-full" style="max-width: 1700px; max-height: auto; overflow: auto;">
         <!-- Modal content -->
-        <br>
 <br>
-
+<br>
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
