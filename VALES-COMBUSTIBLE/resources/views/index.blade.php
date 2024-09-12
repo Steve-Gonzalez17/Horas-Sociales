eb1291ec5886c70fc5774cf2913450b92aa26ccd
Juanjou234 <juanjose.ortizaquino234@gmail.com>
2024-09-11 19:11:01 -0600

    Botones del index

--- VALES-COMBUSTIBLE/resources/views/index.blade.php
+++ VALES-COMBUSTIBLE/resources/views/index.blade.php
@@ -8,6 +8,44 @@
     <title>Document</title>
 </head>
 <body>
+
+    <div style="width: 100%; height: 90%; display: flex; justify-content: center; align-items: center; gap: 60px">
+
+        <a href="#">
+            {{-- button 1 --}}
+    <div class="card card-compact bg-base-100 w-96 shadow-xl button-start">
+        <figure class="img-icon-container">
+          <img
+            src="{{ asset('img/gas-station.png') }}"
+            alt="gas-station" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Ingreso de vales de combustible</h2>
+          <p> &nbsp; a bodega general</p>
+
+        </div>
+      </div>
+      {{-- button 1 end --}}
+        </a>
+
+      <a href="#">
+        {{-- button 2 --}}
+    <div class="card card-compact bg-base-100 w-96 shadow-xl button-start">
+        <figure class="img-icon-container">
+          <img
+            src="{{ asset('img/warehouse-icon-2.png') }}"
+            alt="vouchers" />
+        </figure>
+        <div class="card-body">
+          <h2 class="card-title">Entrega de vales para combustible</h2>
+          <p> &nbsp; (Diesel o gasolina)</p>
+
+        </div>
+      </div>
+      {{-- button 2 end --}}
+      </a>
+</div>
+
 <!-- Main modal -->
 <div id="crud-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
 
@@ -29,7 +67,7 @@
                 </button>
             </div>
             <!-- Modal body -->
-            
+
 <form class="p-4 md:p-5">
     <div class="grid gap-6 mb-6 md:grid-cols-2">
         <div>
@@ -43,15 +81,15 @@
         <div>
             <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
             <input type="text" id="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Flowbite" required />
-        </div>  
-        
+        </div>
+
     </div>
     <div class="mb-6">
         <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
         <input type="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="john.doe@company.com" required />
-    </div> 
-    
-    
+    </div>
+
+
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
 </form>
 
@@ -188,7 +226,7 @@
             </div>
         </div>
     </div>
-</div> 
+</div>
 
 </body>
 </html>
